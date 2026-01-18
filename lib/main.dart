9eeb025292ee16018a3f0cd3fedf081511080af1
AzibMuzammil <azib.muzammil@manafatech.com>
2026-01-18 14:51:14 +0500

    Splash Screen Added

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:e_commerce_app/my_app.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 void main() {
+  /// Widgets Flutter Binding
+  final widgetBinding = WidgetsFlutterBinding.ensureInitialized();
+  /// Flutter Native Splash
+  FlutterNativeSplash.preserve(widgetsBinding: widgetBinding);
   runApp(const MyApp());
 }
