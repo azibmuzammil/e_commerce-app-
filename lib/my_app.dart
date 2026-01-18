9ecc7278cf03925b564d614fb46670f007c808a0
AzibMuzammil <azib.muzammil@manafatech.com>
2026-01-18 12:31:25 +0500

    Utils for theme added

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -1,3 +1,4 @@
+import 'package:e_commerce_app/utils/theme/theme.dart';
 import 'package:flutter/material.dart';
 
 class MyApp extends StatelessWidget {
@@ -5,8 +6,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
+      themeMode: ThemeMode.system,
+      theme: UAppTheme.lightTheme,
+      darkTheme: UAppTheme.darkTheme,
       home: Scaffold(),
     );
   }
